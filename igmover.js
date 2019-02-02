5ddbb8895851ff92012269d8b8f1a4d52357003e
mklyr <chrono@shirosaki.net>
2019-02-02 23:51:15 +0900

    Initial commit

---
+++ igmover.js
@@ -0,0 +1,37 @@
+javascript:(
+function(){
+
+function t(e){
+	var t='http://google.co.jp/maps/@';
+	var n=e.split('; ');
+	for(i=0;i<n.length;i++){
+		if(n[i].match(/ingress\.intelmap\.(lat|lng)=(-?[0-9]+\.[0-9]+)/)){
+			if(RegExp.$1=='lat'){
+				var r = RegExp.$2;
+			}else{
+				var s = RegExp.$2;
+			}
+		}else if(n[i].match(/ingress\.intelmap\.zoom=([0-9]+)/)){
+			var o = RegExp.$1;
+		}
+	}
+	
+	return t + r + ',' + s + ',' + o + 'z';
+}
+
+function n(e){
+	var t = 'http://intel.ingress.com/intel?ll=';
+	if(e.match(/@(-?[0-9]+\.[0-9]+),(-?[0-9]+\.[0-9]+),([0-9]+\.*[0-9]*)z/)){
+		return t += RegExp.$1 + ',' + RegExp.$2 + '&z=' + Math.round(RegExp.$3);
+	}
+}
+
+var e=document.location.href;
+if(e.match(/google.*\/maps/)){
+	location.href = n(e);
+}else if(e.match('ingress.com/intel')){
+	location.href = t(document.cookie);
+}
+
+}
+)()
